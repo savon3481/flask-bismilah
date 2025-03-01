273c8a37f450083ca5660247b68fd4765ad5062b
savon <savon3481@gmail.com>
2025-03-01 16:43:08 +0700

    tambah add user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -12,10 +12,30 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Hubungkan Flask-Migrate dengan Flask dan SQLAlchemy
 
-# Contoh Model
+# Model User
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+
+# Endpoint untuk menambahkan user lewat URL
+@app.route('/adduser/<string:name>', methods=['GET'])
+def add_user(name):
+    if not name:
+        return jsonify({"error": "Nama tidak boleh kosong"}), 400
+
+    new_user = User(name=name)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User berhasil ditambahkan", "user": {"id": new_user.id, "name": new_user.name}}), 201
+
+# Endpoint untuk menampilkan semua user
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{"id": user.id, "name": user.name} for user in users]
+
+    return jsonify({"users": user_list})
 
 @app.route('/')
 def index():
